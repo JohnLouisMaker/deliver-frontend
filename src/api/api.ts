f9591b8946e1b63e5ce24e487d7c5375a8eaf950
JohnLouisMaker <joaoluisdev9@gmail.com>
2026-09-25 09:47:05 -0300

    wip(cart): integrar useCartStore e ação de adicionar produto na HomePage

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,7 +7,7 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 export const api = axios.create({
   baseURL: BASE_URL,
   timeout: 15000,
-  withCredentials: true, // CRÍTICO: Permite o envio de cookies de/para o Render
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
